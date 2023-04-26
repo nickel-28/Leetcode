caf9ef4621358b44cfbc875da1df9edd02f5a109
Nikhil Kumar <84437844+nickel-28@users.noreply.github.com>
2023-04-27 00:12:33 +0530

    Added solution - LeetHub

---
+++ Insert Interval - GFG/insert-interval.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> insertNewEvent(int N, vector<vector<int>> &intervals, vector<int> &newInterval) {
+        // int N = intervals.size();
+        int i = 0;
+        vector<vector<int>> res;
+        //case 1: no overlapping case before the merge intervals
+		//compare ending point of intervals to starting point of newInterval
+        while(i < N && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }                           
+		//case 2: overlapping case and merging of intervals
+        while(i < N && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+        // case 3: no overlapping of intervals after newinterval being merged
+        while(i < N){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> intervals(N,vector<int>(2));
+        for (int i = 0; i < N; i++) {
+            cin >> intervals[i][0]>>intervals[i][1];
+        }
+        vector<int> newEvent(2);
+        cin>>newEvent[0]>>newEvent[1];
+        
+        Solution obj;
+        vector<vector<int>> ans = obj.insertNewEvent(N, intervals,newEvent);
+        cout<<"[";
+        for(int i=0;i<ans.size();i++){
+            cout<<"["<<ans[i][0]<<','<<ans[i][1]<<']';
+            if(i!=(ans.size()-1)) cout<<",";
+        }
+        cout<<"]"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
