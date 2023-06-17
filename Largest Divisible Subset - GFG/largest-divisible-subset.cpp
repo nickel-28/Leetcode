b9f3b459c21106a11a91b36382add7931ed863b1
Nikhil Kumar <84437844+nickel-28@users.noreply.github.com>
2023-06-17 22:32:06 +0530

    Added solution - LeetHub

---
+++ Largest Divisible Subset - GFG/largest-divisible-subset.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> LargestSubset(int n, vector<int>& nums) {
+        vector<int> dp(n,1);
+        vector<int> hash(n);
+        sort(nums.begin(), nums.end());
+        int maxi=1;
+        int lastIndex=0;
+        for(int i=0; i<n; i++){
+            hash[i]=i;
+            for(int prev=0; prev<i; prev++){
+                if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                lastIndex=i;
+            }
+        }
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex=hash[lastIndex];
+            temp.push_back(nums[lastIndex]);
+        }
+        reverse(temp.begin(), temp.end());
+        // for(auto it: temp) cout<<it<<" ";
+        // cout<<endl;
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            arr.push_back(x);
+        }
+
+        Solution obj;
+        vector<int> res = obj.LargestSubset(n, arr);
+        int isValidSeq = 1, sz = res.size();
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                if ((res[i] % res[j]) == 0 || (res[j] % res[i]) == 0)
+                    continue;
+                else {
+                    isValidSeq = 0;
+                    break;
+                }
+            }
+        }
+        cout << isValidSeq << " " << sz << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
