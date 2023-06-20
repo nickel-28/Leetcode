d9fc493ee352ed01b5741152a21adc364e81b4a5
Nikhil Kumar <84437844+nickel-28@users.noreply.github.com>
2023-06-20 13:29:12 +0530

    Added solution - LeetHub

---
+++ Longest String Chain - GFG/longest-string-chain.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool checkPossible(string &s1, string &s2) {
+        if(s1.size() != s2.size() + 1) return false;
+        int first=0, second=0;
+        while(first<s1.size()) {
+            if(s1[first]==s2[second]){
+                first++;
+                second++;
+            }
+            else
+                first++;
+        }
+
+        if(first==s1.size() && second==s2.size()) return true;
+        return false;
+    }
+
+    // comparator function : To sort according to length
+    static bool comp(string &s1, string &s2) {
+        return s1.size() < s2.size();
+    }
+    
+    int longestChain(int n, vector<string>& words) {
+        sort(words.begin(),words.end(), comp);
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=0; i<n; i++){
+            for(int prev=0; prev<i; prev++){
+                if(checkPossible(words[i],words[prev]) && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> words(N);
+        for (int i = 0; i < N; i++) {
+            cin >> words[i];
+        }
+        Solution obj;
+        cout << obj.longestChain(N, words) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
